343f60cb06bd1fdeb4feb91091e867f47f08e21d
MatthewACoelho <49939157+MatthewACoelho@users.noreply.github.com>
2022-02-01 16:55:54 +0000

    Add files via upload

--- app.R
+++ app.R
@@ -21,43 +21,55 @@ xy_str <- function(e) {
 
 # Define UI for application
 ui <- fluidPage(
+    #logos
+    img(height= 55, width = 160, src = "pic2.png"),
+    img(height= 55, width = 160, src = "pic3.png"),
+    br(),
     hr(),
     #title
-    titlePanel("BE-viewer"),
-    hr(),
+    titlePanel("BE-view"),
     #description
     em("interact with base editing mutagenesis data - response to IFNg in HT-29 colorectal cancer cells", 
          p("we used proliferation (cell death) and FACS-based (MHC-I and PDL-1 expression) screening assays",
-           p("CBE: cytosine base editor, ABE: adenine base editor",
+           p("CBE: cytidine base editor, ABE: adenine base editor",
            )
          )
        ),
     br(),
+    #image
+    img(height= 85, width = 455, src = "pic.png"),
+    br(),
+    hr(),
     
     # Sidebar with input 
     sidebarLayout(
         sidebarPanel(
-        selectInput("gene",
-                  "gene", c("JAK1", "STAT1", "IFNGR1", "IFNGR2", "SOCS1", "IRF1", "B2M", "JAK2")),
-        selectInput("screen",
-                  "screen", c("proliferation", "FACS")),
         selectInput("editor",
-                    "editor", c("BE3-NGG (JAK1)", "BE3-NGG (IFNG pathway)", "BE4max-YE1-NGN (JAK1)", "BE3.9max-NGN (JAK1)", "ABE8e-NGN (JAK1)")),
+                        "editor", c("BE3-NGG (IFNG pathway)", "BE3-NGG (JAK1)", "BE4max-YE1-NGN (JAK1)", "BE3.9max-NGN (JAK1)", "ABE8e-NGN (JAK1)")),
+        # Only show gene panel if the pathway screen selected
+        conditionalPanel(
+            condition = "input.editor == 'BE3-NGG (IFNG pathway)'",
+            selectInput("gene",
+                        "gene", c("JAK1", "STAT1", "IFNGR1", "IFNGR2", "SOCS1", "IRF1", "B2M", "JAK2")),
+            ),
+        # Only show screen panel if the available editor screens are selected
+        conditionalPanel(
+            condition = "input.editor == 'BE3-NGG (IFNG pathway)'| input.editor == 'BE3-NGG (JAK1)'",
+            selectInput("screen",
+                         "screen", c("proliferation", "FACS")),
+        ),
         selectInput("labels",
                     "labels", c("predicted amino acid change", "PTMs", "associated phenotypes", "citations")),
         hr(),
         downloadButton("downloadData", "download results"),
+        width = 9,
         ),
         
         #MAIN
         mainPanel(
-            br(),
-            #image
-            img(height= 85, width = 455, src = "pic.png"),
-            br(),
-            hr(),
             
             #text
+            hr(),
             textOutput("text"),
             
             #results
@@ -72,7 +84,7 @@ ui <- fluidPage(
             br(),
             hr(),
             h6("PTMs: post-translational modifications"),
-            h6("gRNA off target summary: number of GRCh38 genomic positions with 0, 1, 2, 3, or 4 mismatches"),
+            h6("gRNA off target summary: number of GRCh38 genomic positions with 0, 1, 2, 3, or 4 mismatches (please download data for this information)"),
             
             #MIT license etc with footnotes
             hr(),
@@ -83,12 +95,13 @@ ui <- fluidPage(
             br(),
             hr(),
             h5("License"),
-            h6("Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+            h6("MIT: Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
             The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
             THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."),
             hr(),
             h6("Shiny app development by Matt Coelho. Analysis by Matt Coelho. We used VEP for base editing predictions"),
             a("VEP", href = "https://www.ensembl.org/info/docs/tools/vep/index.html"),
+            width = 9
          
         )
     )
@@ -104,9 +117,14 @@ server <- function(input, output) {
     results <- reactive({
                 data <- data %>% filter(Gene == input$gene) %>%
                     filter(editor == input$editor) %>%
-                    select(!c(Amino_Acid_Position_simple))
+                    select(!c(Amino_Acid_Position_simple, sgRNA_ID, off_target_summary_NGN, off_target_summary_NGG))
     })
-    
+
+    results_download <- reactive({
+        data <- data %>% filter(Gene == input$gene) %>%
+            filter(editor == input$editor) %>%
+            select(!c(Amino_Acid_Position_simple))
+    })
     
     
     #Render functions
@@ -243,7 +261,7 @@ server <- function(input, output) {
             paste(c(input$gene, "_mutagenesis.txt"), collapse = "")
         },
         content = function(file) {
-            write.table(results(), file, sep ="\t")
+            write.table(results_download(), file, sep ="\t")
         }
     )
 }
